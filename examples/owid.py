8391abf851f75cb0872e2200944d74cf96715cef
odadoun <odadoun@gmail.com>
2025-03-21 12:30:43 +0100

    add example

---
+++ examples/owid.py
@@ -0,0 +1,11 @@
+import pyvoa.front as pv
+import matplotlib
+matplotlib.use('Agg')
+
+def test():
+    pv.setwhom('owid',reload=False)
+    pv.setvisu(vis='matplotlib')
+    pv.map(where='Europe')
+    return pv
+pl = test()
+pl.savefig('mapspf.png')
