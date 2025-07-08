3384862eae90138eea545215504a1ff84bf7ad1e
Tristan Beau <tristan.beau@univ-paris-diderot.fr>
2025-07-08 11:23:56 +0200

    preparing v0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyvoa",  
-    version="0.3.0",  
+    version="0.3.1",  
     author="pyvoa.org",
     author_email="contact@pyvoa.org",
     description="Python virus open analysis. See more on pyvoa.org",
