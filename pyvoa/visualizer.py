ec4bc07f892b0cfe5cc16ded17ed8fc71a445baa
odadoun <odadoun@gmail.com>
2025-11-21 16:24:13 +0100

    change color of the warning msg

--- pyvoa/visualizer.py
+++ pyvoa/visualizer.py
@@ -298,7 +298,7 @@ class AllVisu:
         input = kwargs.get('input')
         if vis != 'bokeh' and kwargs['dateslider']:
             kwargs.pop("dateslider")
-            PyvoaError("Only avalaible for vis='bokeh' dummy argument")
+            PyvoaWarning("Only avalaible for vis='bokeh' dummy argument")
         if 'geometry' not in list(input.columns):
             raise PyvoaError('No geometry in you pandas, map can not be called ...')
         if vis == 'matplotlib':
