cd3bb20d1a0e300ed24d52a0b40c6302310cfa12
odadoun <odadoun@gmail.com>
2026-02-11 15:09:21 +0100

    change setvisu to setvis

--- examples/pyfiles/owid.py
+++ examples/pyfiles/owid.py
@@ -1,10 +1,12 @@
 import pyvoa.front as pv
+import pyvoa.tools as pt
 import matplotlib
 matplotlib.use('Agg')
 
+pt.set_verbose_mode(2)
 def test():
     pv.setwhom('owid',reload=False)
-    pv.setvisu(vis='matplotlib')
+    pv.setvis(vis='matplotlib')
     pv.map(where='Europe')
     return pv
 
