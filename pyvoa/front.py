c4c5c00ae02c8809754b69c98c21eb1b0ef08f36
Tristan Beau <tristan.beau@univ-paris-diderot.fr>
2026-06-01 11:38:31 +0200

    suppress jupyter warning for datetime, which is due to external module. New try

--- pyvoa/front.py
+++ pyvoa/front.py
@@ -15,7 +15,8 @@ Date :    April 2020 - November 2025
 # --- Imports ----------------------------------------------------------
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
-warnings.filterwarnings("ignore", category=DeprecationWarning, module='jupyter_client')
+#warnings.filterwarnings("ignore", category=DeprecationWarning, module='jupyter_client')
+warnings.simplefilter(action="ignore", category=DeprecationWarning, module='jupyter_client')
 
 import pandas as pd
 from functools import wraps
