285fc8467019c8ce89ba19582857bb45bca603ce
odadoun <odadoun@gmail.com>
2026-08-27 15:44:22 +0200

    fix a bug in get

--- pyvoa/front.py
+++ pyvoa/front.py
@@ -788,11 +788,10 @@ class front:
         hand the undecorated body the assembled table. :meth:`whattodo` lists every
         argument together with the values it accepts.
         """
-        # columns=list(kwargs['input'].columns)  # fed the keepalso list, dropped
-        # when get() started keeping only the indicator that was asked for
+        columns=list(kwargs['input'].columns) 
         if kwargs['kwargsuser']['what'] != 'current':
             kwargs['which'] = kwargs['what']
-        tokeep = ['date', 'where', 'code'] + kwargs['which'] + (['geometry'] if 'geometry' in columns else [])    
+        tokeep = ['date', 'where', 'code'] + kwargs['which'] + (['geometry'] if 'geometry' in columns else [])
         return kwargs['input'][tokeep]
 
     def decomap(func):
